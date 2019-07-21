708f818b48c4da900a7099cf953ac51330d41308
larryaina1993 <larryaina1993@gmail.com>
2019-07-21 02:22:02 +0100

    Prepared for deployment

--- routes/post.js
+++ routes/post.js
@@ -6,7 +6,13 @@ const {
   postById,
   isPoster,
   updatePost,
-  deletePost
+  deletePost,
+  photo,
+  singlePost,
+  like,
+  unlike,
+  comment,
+  uncomment
 } = require('../controllers/post');
 const { requireSignin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
@@ -15,15 +21,27 @@ const { createPostValidator } = require('../validator');
 const router = express.Router();
 
 router.get('/posts', getPosts);
+
+//like unlike
+router.put('/post/like', requireSignin, like);
+router.put('/post/unlike', requireSignin, unlike);
+
+// comments
+router.put('/post/comment', requireSignin, comment);
+router.put('/post/uncomment', requireSignin, uncomment);
+
 router.post(
   '/post/new/:userId',
   requireSignin,
   createPost,
   createPostValidator
 );
-router.get('/posts/by/:userId', postsByUser);
+router.get('/posts/by/:userId', requireSignin, postsByUser);
+router.get('/post/:postId', singlePost);
 router.put('/post/:postId', requireSignin, isPoster, updatePost);
 router.delete('/post/:postId', requireSignin, isPoster, deletePost);
+//photo
+router.get('/post/photo/:postId', photo);
 
 //any route containing :userId, our app will first execute userById()
 router.param('userId', userById);
